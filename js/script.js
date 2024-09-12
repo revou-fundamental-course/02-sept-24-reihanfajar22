07dfa034ccba1b8ab489dd93f17fb8870af73190
reihanfajar22 <raihanfajar606@gmail.com>
2024-09-12 08:56:27 +0700

    finaly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 function hitung() {
-  var alas = document.getElementById("alas");
-  console.log(alas);
+  let sisi = document.getElementById("sisi").value;
+
+  if (sisi == "") {
+    alert("di isi dulu");
+  }
+
+  let hasil = parseInt(sisi) * 3;
+  console.log(hasil);
+  document.writeln("Hasilnya adalah" + " " + hasil).innerHTML = hasil;
+}
+
+function luas() {
+  let alas = document.getElementById("alas").value;
+  let tinggi = document.getElementById("tinggi").value;
+
+  if (alas == "" || tinggi == "") {
+    alert("di isi dulu");
+  }
+
+  let hasil = parseInt(alas) * parseInt(tinggi) * 0.5;
+  console.log(hasil);
+  document.writeln("Hasilnya adalah" + " " + hasil);
 }
